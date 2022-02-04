a427609f7893790e5d7fa4a96d86611b4d375b90
Lawrence L <LawrenceELee@gmail.com>
2022-02-03 22:23:25 -0800

    Got the code skeleton ready. Need to implement the show, update, and delete CRUD operations for facts and members.

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,13 @@ Rails.application.routes.draw do
 
   get '/test', to: 'test#show'
 
+
+  namespace :api do
+    namespace :v1 do
+      resources :members do
+        resources :facts
+      end
+    end
+  end
+
 end
